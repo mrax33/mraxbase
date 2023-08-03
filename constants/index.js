c1b5e59d56bcbd8f3b41c10be5bb62d29a0e285c
mrax33 <95886626+mrax33@users.noreply.github.com>
2023-08-03 21:08:05 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbed33b0cb51b71ddc74158345a1103fab949a103";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
